6d05141bc684c9dc9414fdf0a7549ea73e471c3d
Prezzi25742 <patricknze577@gmail.com>
2026-04-29 20:01:52 +0100

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -38,7 +38,7 @@ export async function handleBookingForm(data: BookingData): Promise<ActionRespon
           time_preference: data.time,
           massage_type: data.massage,
           addon: data.addon || "None",
-          guests: parseInt(data.people, 10),
+          guests: data.people,
         },
       ]);
 
